2db196d0b3fdeb3cd327169a949039c01ebc92ca
Marcos Ferreira <marcosferreira.contato@gmail.com>
2019-07-28 16:43:19 +0000

    criptografando password e exibindo diferente mensagem de erro em caso de registro de usuário.

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('../database');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -22,6 +23,12 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
+UserSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+
+    next();
+})
 
 const User = mongoose.model('User', UserSchema);
 
