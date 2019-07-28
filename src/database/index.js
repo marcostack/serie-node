cf20ca91b598a0a8375cd6abec52f077fee8754c
Marcos Ferreira <marcosferreira.contato@gmail.com>
2019-07-28 00:05:19 +0000

    novo comentario de teste para commit

--- src/database/index.js
+++ src/database/index.js
@@ -3,4 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/noderest', { useNewUrlParser: true, useCreateIndex: true });
 mongoose.Promise = global.Promise;
 
+// comentario de teste
+
 module.exports = mongoose;
