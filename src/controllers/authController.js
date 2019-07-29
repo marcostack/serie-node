7a625044a653763f14b314d11c3a9c6568f1a648
Marcos Ferreira <marcosferreira.contato@gmail.com>
2019-07-29 09:35:58 +0000

    implementando token, também na parte de registro de usuário

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,6 +8,12 @@ const User = require('../models/User');
 
 const router = express.Router();
 
+function generateToken(params = {}) {
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400, //um dia em segundos
+    });
+}
+
 router.post('/register', async (req, res) => {
     const { email } = req.body;
 
@@ -19,7 +25,10 @@ router.post('/register', async (req, res) => {
 
         user.password = undefined;
 
-        return res.send({ user });
+        return res.send({
+            user,
+            token: generateToken({ id: user.id }),
+        });
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed' });
     }
@@ -38,11 +47,10 @@ router.post('/authenticate', async (req, res) => {
 
     user.password = undefined;
 
-    const token = jwt.sign({ id: user._id }, authConfig.secret, {
-        expiresIn: 86400,
+    res.send({ 
+        user,
+        token: generateToken({ id: user.id }), 
     });
-
-    res.send({ user, token });
 });
 
 module.exports = app => app.use('/auth', router);
