52f267fe8ded484aa009d605a1b79c5a677d95be
Marcos Ferreira <marcosferreira.contato@gmail.com>
2019-07-29 15:13:19 +0000

    implementaçã do controller project, para logar usuário assim que se registrar

--- src/index.js
+++ src/index.js
@@ -9,5 +9,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.get('/', (req, res) => res.send('Olá Mundo!'));
 
 require('./controllers/authController')(app);
+require('./controllers/projectController')(app);
 
 app.listen(3000, () => console.log(`execultando servidor da api...`));
